c65a10a5e9f59fa31175191b68ce1cfdc13a2300
Joe Gelay <joegelay@gmail.com>
2020-01-30 20:57:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -128,7 +128,9 @@ def num_points_scored(players_name)
  game_hash.each { |place, team| 
    team.each { |attribute, data| 
      if attribute == :players
-       data.each { |
+       data.each { |player| 
+         
+       }
      end 
    }
  }
