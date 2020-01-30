db2a46df50978922733382ca85ee4ccdb0d21cfd
Joe Gelay <joegelay@gmail.com>
2020-01-30 22:44:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -170,7 +170,6 @@ def player_numbers(sought_team)
    game_hash.each { |place, team| 
      if team[:team_name] == sought_team
        team.each { |attribute, data| 
-      
        if attribute == :players
         data.each { |player| 
         numbers << player[:number]
