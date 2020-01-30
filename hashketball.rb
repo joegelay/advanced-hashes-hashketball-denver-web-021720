bb6d816db75e3616dbe48e71d1f99a6e24b0d2f7
Joe Gelay <joegelay@gmail.com>
2020-01-30 23:28:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -268,10 +268,6 @@ def winning_team
    end 
   }
   
-  if home_total_points > away_total_points ? home_team : away_team
-    return home_team
-  else 
-    return away_team
-  end 
+  home_total_points > away_total_points ? home_team : away_team
 end 
 
